b9aa9eb80c5f74b017abd550bfcec581f1615f52
Vaishali nile <vaishalinile896@gmail.com>
2024-09-25 00:49:11 +0530

    server link added

--- src/Componenets/Find-Stations/findstations.jsx
+++ src/Componenets/Find-Stations/findstations.jsx
@@ -1,46 +1,49 @@
 import React from "react";
 
 import Nav from "../Home/Nav";
-import SearchBox from "./searchbox.jsx"
+import SearchBox from "./searchbox.jsx";
 import Stations from "./stations.jsx";
 import Footer from "../Home/footer.jsx";
 import Googlemap from "./googlemap.jsx";
-import { useState , useEffect } from "react";
+import { useState, useEffect } from "react";
 
+function Find_Stations() {
+  const [filteredStations, setFilteredStations] = useState([]);
+  const [stations, setStations] = useState([]);
 
-function Find_Stations(){
-    const [filteredStations, setFilteredStations] = useState([]);
-    const [stations, setStations] = useState([]);
-
-    useEffect(() => {
-        async function fetchStations() {
-            try {
-                const response = await fetch("http://localhost:5038/Backend/test/stations/");
-                const data = await response.json();
-                setStations(data);
-            } catch (error) {
-                console.error("Error fetching stations:", error);
-            }
-        }
-        fetchStations();
-    }, []);
-    // Function to handle search action
-    const handleSearch = (searchText) => {
-        // Perform search logic to filter stations based on searchText
-        const filteredData = stations.filter(station =>
-            station.Address1.toLowerCase().includes(searchText.toLowerCase())
+  useEffect(() => {
+    async function fetchStations() {
+      try {
+        const response = await fetch(
+          "https://chargemate-backend-1.onrender.com/Backend/test/stations/"
         );
-        setFilteredStations(filteredData);
-    };
-    return(
-        <>
-        <Nav/>
-        <SearchBox onSearch={handleSearch} />
-        <Googlemap/>
-        <Stations stations={filteredStations.length > 0 ? filteredStations : stations} />
-        <Footer/>
-        </>
+        const data = await response.json();
+        setStations(data);
+      } catch (error) {
+        console.error("Error fetching stations:", error);
+      }
+    }
+    fetchStations();
+  }, []);
+  // Function to handle search action
+  const handleSearch = (searchText) => {
+    // Perform search logic to filter stations based on searchText
+    const filteredData = stations.filter((station) =>
+      station.Address1.toLowerCase().includes(searchText.toLowerCase())
     );
+    setFilteredStations(filteredData);
+  };
+  return (
+    <>
+      <Nav />
+      <SearchBox onSearch={handleSearch} />
+      <Googlemap />
+      <Stations
+        stations={filteredStations.length > 0 ? filteredStations : stations}
+      />
+      <Footer />
+    </>
+  );
 }
 
 export default Find_Stations;
